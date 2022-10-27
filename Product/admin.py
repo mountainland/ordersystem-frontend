3e759a0f9f6c0cca90358b415eb0e9921f90d012
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-10-27 11:09:27 +0300

    Changes

--- Product/admin.py
+++ Product/admin.py
@@ -11,8 +11,8 @@ admin.site.register(Product, ProductAdmin)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['product','count','cost','order_by', 'name','address', 'postcode', 'city', 'delivered', 'delivered_on', 'created', 'last_updated']
-    readonly_fields = ['slug','order_by', 'created', 'last_updated',]
+    list_display = ['product','count','cost','order_by', 'name','address', 'postcode', 'city', 'delivered', 'delivered_on', 'created', 'last_updated', 'information']
+    readonly_fields = ['slug','order_by', 'created', 'last_updated', 'payment_method']
 
 admin.site.register(Order, OrderAdmin)
 
