a290bd4e58acc101632f2438b0f068fcaf41786b
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-10-30 18:57:13 +0200

    Orders admin

--- Product/urls.py
+++ Product/urls.py
@@ -10,7 +10,7 @@ urlpatterns = (
     path('product/detail/<slug:slug>/', views.ProductDetailView.as_view(), name='Product_product_detail'),
     # urls for order
     path('orders/', views.OrderListView.as_view(), name='Product_order_list'),
-    path('orders_admin/', views.OrderAdminView.as_view(), name='Product_orders_admin'),
+    path('orders/admin/', views.OrderAdminView.as_view(), name='Product_orders_admin'),
     path('order/conformed/', views.order_conform, name='Product_order_conform'),
     path('order/create/', views.OrderCreateView.as_view(), name='Product_order_create'),
     path('order/detail/<slug:slug>/', views.OrderDetailView.as_view(), name='Product_order_detail'),
