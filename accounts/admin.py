56de2917414c6ba0b886ff163d4fa5cfa33bb911
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-10-31 17:40:31 +0200

    bug fixing

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from . models import Code
 
 # Register your models here.
 class CodesAdmin(admin.ModelAdmin):
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,29 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import Code
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+USERMODEL = get_user_model()
 
 def SignUp(request):
     if (request.POST):
-        signup_data = request.POST.dict()
-        username = signup_data.get("username")
-        password = signup_data.get("password")
-        signup_code = signup_data.get("signup_code")
-        if Code.objects.filter(code=signup_code).exists():
-            user = User.objects.create_user(username, 'NOTAMAIL@MAIL.COM', password)
-            user.save()
-        return reverse_lazy('login')
+        signup_data = request.POST
+        username1 = signup_data["username"]
+        password1 = signup_data["password1"]
+        password2 = signup_data["password2"]
+        if password1 == password2:
+            password = password1
+        else:
+            return render(request, 'registration/signup.html')
+        signup_code = signup_data["signup_code"]
+        if Code.objects.filter(code=signup_code, used=False).exists():
+            USERMODEL.objects.create_user(username=username1, password=password)
+            code = Code.objects.get(code=signup_code)
+            code.used = True
+            code.save()
+        return redirect('Product_order_create')
     else:
         return render(request, 'registration/signup.html')
