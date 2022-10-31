d631d06658bde924687842384d51d1f4266c5ac6
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-10-31 18:18:50 +0200

    Changed defaults

--- accounts/models.py
+++ accounts/models.py
@@ -4,14 +4,3 @@ class Code(models.Model):
     code = models.CharField(max_length=8)
     created = models.DateTimeField(auto_now_add=True)
     used = models.BooleanField(default=False)
-
-    def __str__(self):
-        return 
-
-    def __unicode__(self):
-        return 
-
-    def used(self, *args, **kwargs)
-        if not self.used:
-            self.used = True
-        super(Code, self).save(*args, **kwargs)
